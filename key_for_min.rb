3595d67e677c1699486d30175a8b1f046096cbef
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-06 21:25:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,8 @@ def key_for_min_value(name_hash)
     
     if value < smallest
       value = smallest
-      name = name_for
+      name = name_for.to_s
     end
   end
-  return name_for
+  return name_for.to_s
 end
